67e7957d04168d54d43366b525421020fef55921
John <jpena91@gmail.com>
2019-07-02 14:56:57 -0400

    user controller edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,13 @@ class UsersController < ApplicationController
 		end
 	end
 
+  def update
+    user = User.find(params[:id])
+
+    user.update(params)
+
+    render json: user
+  end
 
   def show
 		user = User.find(params[:id])

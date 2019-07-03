50efab96f0567d07fe80b28c8640f9afe01fe16e
John <jpena91@gmail.com>
2019-07-03 10:05:08 -0400

    change auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,23 +1,26 @@
 class AuthController < ApplicationController
 
   def login
+
     # Find the user by their username
     # Who do you claim to be?
     user = User.find_by(username: params[:username])
 
     # Check their submitted password against the password digest
     # Are you really this person?
-    # if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
     #   # If worked, send back the user
     #   token = encode_token(user)
-    #
-    #   render json: {user: User.new(user), token: token}
-    # else
-    #   # If not, tell them they screwed up
-    #   render json: {errors: "Wrong username/password!"}
-    # end
 
-    render json: user && user.authenticate(params[:password])
+      render json: user
+      # render json: {user: User.new(user), token: token}
+#
+    else
+    #   # If not, tell them they screwed up
+      render json: {errors: "Wrong username/password!"}
+    # end
+  end
+    # render json: user && user.authenticate(params[:password])
   end
 
   def auto_login
@@ -30,4 +33,5 @@ class AuthController < ApplicationController
     end
 
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,21 +33,14 @@ class UsersController < ApplicationController
       bmi: params[:bmi],
 		)
 		if user.save
-
+			# token = encode_token(user)
 
 			render json: user
 		else
-			render json: (user.errors.full_messages)
+			render json: {errors: user.errors.full_messages}
 		end
 	end
 
-  def update
-    user = User.find(params[:id])
-
-    user.update(params)
-
-    render json: user
-  end
 
   def show
 		user = User.find(params[:id])
